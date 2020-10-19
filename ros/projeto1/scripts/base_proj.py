127583a1ea2cc40eed967cb4fa10df016c2552a1
mirwox <fmiranda@gmail.com>
2020-10-19 14:08:07 -0300

    remocao alvar

--- ros/projeto1/scripts/base_proj.py
+++ ros/projeto1/scripts/base_proj.py
@@ -17,7 +17,7 @@ from tf import transformations
 from tf import TransformerROS
 import tf2_ros
 from geometry_msgs.msg import Twist, Vector3, Pose, Vector3Stamped
-from ar_track_alvar_msgs.msg import AlvarMarker, AlvarMarkers
+
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Header
 
@@ -52,45 +52,6 @@ frame = "camera_link"
 tfl = 0
 
 tf_buffer = tf2_ros.Buffer()
-
-
-def recebe(msg):
-    global x # O global impede a recriacao de uma variavel local, para podermos usar o x global ja'  declarado
-    global y
-    global z
-    global id
-    for marker in msg.markers:
-        id = marker.id
-        marcador = "ar_marker_" + str(id)
-
-        print(tf_buffer.can_transform(frame, marcador, rospy.Time(0)))
-        header = Header(frame_id=marcador)
-        # Procura a transformacao em sistema de coordenadas entre a base do robo e o marcador numero 100
-        # Note que para seu projeto 1 voce nao vai precisar de nada que tem abaixo, a 
-        # Nao ser que queira levar angulos em conta
-        trans = tf_buffer.lookup_transform(frame, marcador, rospy.Time(0))
-        
-        # Separa as translacoes das rotacoes
-        x = trans.transform.translation.x
-        y = trans.transform.translation.y
-        z = trans.transform.translation.z
-        # ATENCAO: tudo o que vem a seguir e'  so para calcular um angulo
-        # Para medirmos o angulo entre marcador e robo vamos projetar o eixo Z do marcador (perpendicular) 
-        # no eixo X do robo (que e'  a direcao para a frente)
-        t = transformations.translation_matrix([x, y, z])
-        # Encontra as rotacoes e cria uma matriz de rotacao a partir dos quaternions
-        r = transformations.quaternion_matrix([trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z, trans.transform.rotation.w])
-        m = numpy.dot(r,t) # Criamos a matriz composta por translacoes e rotacoes
-        z_marker = [0,0,1,0] # Sao 4 coordenadas porque e'  um vetor em coordenadas homogeneas
-        v2 = numpy.dot(m, z_marker)
-        v2_n = v2[0:-1] # Descartamos a ultima posicao
-        n2 = v2_n/linalg.norm(v2_n) # Normalizamos o vetor
-        x_robo = [1,0,0]
-        cosa = numpy.dot(n2, x_robo) # Projecao do vetor normal ao marcador no x do robo
-        angulo_marcador_robo = math.degrees(math.acos(cosa))
-
-        # Terminamos
-        print("id: {} x {} y {} z {} angulo {} ".format(id, x,y,z, angulo_marcador_robo))
 
 
 
@@ -135,7 +96,6 @@ if __name__=="__main__":
     topico_imagem = "/camera/image/compressed"
 
     recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
-    recebedor = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, recebe) # Para recebermos notificacoes de que marcadores foram vistos
 
 
     print("Usando ", topico_imagem)
