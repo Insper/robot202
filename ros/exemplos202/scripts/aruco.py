e84a9cb3a5e281de47b9c6bb92ec5d68757233fc
arnaldo <arnaldoavj@insper.edu.br>
2020-10-20 11:01:23 -0300

    calculo distancia

--- ros/exemplos202/scripts/aruco.py
+++ ros/exemplos202/scripts/aruco.py
@@ -1,60 +1,22 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
 
-
-
-
 import rospy
 import numpy as np
-import tf
 import math
 import cv2
 import time
 from geometry_msgs.msg import Twist, Vector3, Pose
-from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Image, CompressedImage
+from sensor_msgs.msg import Image, CompressedImage, LaserScan
 from cv_bridge import CvBridge, CvBridgeError
-import cormodule
 
 import cv2.aruco as aruco
 import sys
 
 #--- Define Tag de teste
-id_to_find  = 13
-marker_size  = 10 #- [cm]
-
-#------------------------------------------------------------------------------
-#------- ROTATIONS https://www.learnopencv.com/rotation-matrix-to-euler-angles/
-#------------------------------------------------------------------------------
-# Checks if a matrix is a valid rotation matrix.
-def isRotationMatrix(R):
-    Rt = np.transpose(R)
-    shouldBeIdentity = np.dot(Rt, R)
-    I = np.identity(3, dtype=R.dtype)
-    n = np.linalg.norm(I - shouldBeIdentity)
-    return n < 1e-6
-
-
-# Calculates rotation matrix to euler angles
-# The result is the same as MATLAB except the order
-# of the euler angles ( x and z are swapped ).
-def rotationMatrixToEulerAngles(R):
-    assert (isRotationMatrix(R))
-
-    sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
-
-    singular = sy < 1e-6
-
-    if not singular:
-        x = math.atan2(R[2, 1], R[2, 2])
-        y = math.atan2(-R[2, 0], sy)
-        z = math.atan2(R[1, 0], R[0, 0])
-    else:
-        x = math.atan2(-R[1, 2], R[1, 1])
-        y = math.atan2(-R[2, 0], sy)
-        z = 0
-
-    return np.array([x, y, z])
+id_to_find  = 200
+marker_size  = 11.5 #- [cm]
+# 
 
 
 #--- Get the camera calibration path
@@ -62,28 +24,30 @@ calib_path  = "/home/borg/catkin_ws/src/robot202/ros/exemplos202/scripts/"
 camera_matrix   = np.loadtxt(calib_path+'cameraMatrix_raspi.txt', delimiter=',')
 camera_distortion   = np.loadtxt(calib_path+'cameraDistortion_raspi.txt', delimiter=',')
 
-#--- 180 deg rotation matrix around the x axis
-R_flip  = np.zeros((3,3), dtype=np.float32)
-R_flip[0,0] = 1.0
-R_flip[1,1] =-1.0
-R_flip[2,2] =-1.0
-
 #--- Define the aruco dictionary
-aruco_dict  = aruco.getPredefinedDictionary(aruco.DICT_5X5_1000)
+aruco_dict  = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
 parameters  = aruco.DetectorParameters_create()
+#parameters.minDistanceToBorder = 0
+#parameters.adaptiveThreshWinSizeMax = 1000
 
 #-- Font for the text in the image
 font = cv2.FONT_HERSHEY_PLAIN
 
 bridge = CvBridge()
 cv_image = None
-
+scan_dist = 0
+def scaneou(dado):
+	#print("scan")
+	global scan_dist 
+	scan_dist = dado.ranges[0]*100
+	#print("scan", scan_dist)
+	return scan_dist
 
 
 # A função a seguir é chamada sempre que chega um novo frame
 def roda_todo_frame(imagem):
-	print("frame")
-
+	#print("frame")
+	
 	try:
 		cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
 		gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
@@ -100,40 +64,35 @@ def roda_todo_frame(imagem):
 			rvec, tvec = ret[0][0,0,:], ret[1][0,0,:]
 
 			#-- Draw the detected marker and put a reference frame over it
-			aruco.drawDetectedMarkers(cv_image, corners)
-			aruco.drawAxis(cv_image, camera_matrix, camera_distortion, rvec, tvec, 10)
+			aruco.drawDetectedMarkers(cv_image, corners, ids)
+			aruco.drawAxis(cv_image, camera_matrix, camera_distortion, rvec, tvec, 1)
+			
+			# Calculo usando distancia Euclidiana 
+			distance = np.sqrt(tvec[0]**2 + tvec[1]**2 + tvec[2]**2)
 
 			#-- Print the tag position in camera frame
-			str_position = "MARKER Position x=%4.0f  y=%4.0f  z=%4.0f"%(tvec[0], tvec[1], tvec[2])
-			cv2.putText(cv_image, str_position, (0, 100), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+			#str_position = "Marker x=%4.0f  y=%4.0f  z=%4.0f"%(tvec[0], tvec[1], tvec[2])
+			#print(str_position)
+			#cv2.putText(cv_image, str_position, (0, 30), font, 1, (0, 255, 0), 1, cv2.LINE_AA)
 
-			#-- Obtain the rotation matrix tag->camera
-			R_ct    = np.matrix(cv2.Rodrigues(rvec)[0])
-			R_tc    = R_ct.T
+			#-- Print the tag position in camera frame
+			str_dist = "Dist aruco=%4.0f  scan=%4.0f"%(distance, scan_dist)
+			print(str_dist)
+			cv2.putText(cv_image, str_dist, (0, 15), font, 1, (0, 255, 0), 1, cv2.LINE_AA)
 
-			#-- Get the attitude in terms of euler 321 (Needs to be flipped first)
-			roll_marker, pitch_marker, yaw_marker = rotationMatrixToEulerAngles(R_flip*R_tc)
+			# calculo usando a distancia focal da camera 
+			f = 1073.45 #distancia focal da raspicam
+			#print(corners)
+			y_top = (corners[0][0][2][1] + corners[0][0][3][1] ) / 2
+			y_bottom = (corners[0][0][0][1] + corners[0][0][1][1] ) / 2
+			height = (y_top - y_bottom) # in pixels
+			distance_away = f * ( 7.1 / height )
+			str_dist2 = "Dist pix=%4.0f"%(distance_away)
+			cv2.putText(cv_image, str_dist2, (0, 30), font, 1, (0, 255, 0), 1, cv2.LINE_AA)			
+			cv2.putText(cv_image, "Un. cm", (0, 45), font, 1, (0, 255, 0), 1, cv2.LINE_AA)
 
-			#-- Print the marker's attitude respect to camera frame
-			str_attitude = "MARKER Attitude r=%4.0f  p=%4.0f  y=%4.0f"%(math.degrees(roll_marker),math.degrees(pitch_marker),
-								math.degrees(yaw_marker))
-			cv2.putText(cv_image, str_attitude, (0, 150), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
-
-
-			#-- Now get Position and attitude f the camera respect to the marker
-			pos_camera = -R_tc*np.matrix(tvec).T
-
-			str_position = "CAMERA Position x=%4.0f  y=%4.0f  z=%4.0f"%(pos_camera[0], pos_camera[1], pos_camera[2])
-			cv2.putText(cv_image, str_position, (0, 200), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
-
-			#-- Get the attitude of the camera respect to the frame
-			roll_camera, pitch_camera, yaw_camera = rotationMatrixToEulerAngles(R_flip*R_tc)
-			str_attitude = "CAMERA Attitude r=%4.0f  p=%4.0f  y=%4.0f"%(math.degrees(roll_camera),math.degrees(pitch_camera),
-								math.degrees(yaw_camera))
-			cv2.putText(cv_image, str_attitude, (0, 250), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
-
-
-		cv2.imshow("Camera", cv_image)
+		img_out = cv2.resize(cv_image, (640, 480))
+		cv2.imshow("Camera", img_out)
 		cv2.waitKey(1)
 	except CvBridgeError as e:
 		print('ex', e)
@@ -143,12 +102,14 @@ if __name__=="__main__":
 
 	topico_imagem = "/camera/image/compressed"
 
-
 	recebe_imagem = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
 	print("Usando ", topico_imagem)
 
 	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
-
+	
+	recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
+	
+	
 	try:
 
 		while not rospy.is_shutdown():
